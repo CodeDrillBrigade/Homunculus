5fa9e89f149a0ac1e54e59cbd161522a05407ac0
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2023-11-11 13:06:10 +0100

    Small fix

--- src/main/kotlin/org/cdb/labwitch/LabWitchApp.kt
+++ src/main/kotlin/org/cdb/labwitch/LabWitchApp.kt
@@ -5,11 +5,6 @@ import org.cdb.labwitch.configuration.configureHTTP
 import org.cdb.labwitch.configuration.configureKoin
 import org.cdb.labwitch.configuration.configureRouting
 
-// fun main() {
-//     embeddedServer(CIO, port = 8080, host = "0.0.0.0", module = Application::module)
-//         .start(wait = true)
-// }
-
 fun main(args: Array<String>) = io.ktor.server.cio.EngineMain.main(args)
 
 fun Application.module() {
