b891ece3831adc46bfe7cdca959671b57211c799
Vincenzo Pierro <vcp@icure.com>
2024-09-02 20:05:27 +0200

    :loud_sound: Added logging on 500 exception

--- src/main/kotlin/org/cdb/homunculus/configuration/ExceptionConfig.kt
+++ src/main/kotlin/org/cdb/homunculus/configuration/ExceptionConfig.kt
@@ -4,10 +4,14 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
+import io.ktor.util.logging.KtorSimpleLogger
+import io.ktor.util.logging.error
 import org.cdb.homunculus.exceptions.NotFoundException
 import org.cdb.homunculus.exceptions.UnauthorizedException
 import org.cdb.homunculus.models.security.StatusResponse
 import java.io.IOException
+
+private val logger = KtorSimpleLogger("com.example.RequestTracePlugin")
 
 fun Application.configureExceptions() {
 	fun Exception.toErrorResponse(status: HttpStatusCode) =
@@ -26,11 +30,13 @@ fun Application.configureExceptions() {
 				is IllegalArgumentException -> call.respond(HttpStatusCode.BadRequest, cause.toErrorResponse(HttpStatusCode.BadRequest))
 				is UnauthorizedException -> call.respond(HttpStatusCode.Unauthorized, cause.toErrorResponse(HttpStatusCode.Unauthorized))
 				is NotFoundException -> call.respond(HttpStatusCode.NotFound, cause.toErrorResponse(HttpStatusCode.NotFound))
-				else ->
+				else -> {
+					logger.error(cause)
 					call.respond(
 						HttpStatusCode.InternalServerError,
 						StatusResponse(false, cause.message ?: "Something went wrong", HttpStatusCode.InternalServerError.value),
 					)
+				}
 			}
 		}
 	}
